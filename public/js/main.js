9061001679952169f916dcd0ba04b383f14512aa
Klout <55015851+KloutDevs@users.noreply.github.com>
2021-01-21 11:43:51 -0300

    Updated source

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,32 @@
 const { ipcRenderer, Notification, remote} = require('electron');
+var timestamp = false;
 
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.title-bar').classList.add('animate__animated');
+    document.querySelector('.title-bar').classList.add('animate__backInLeft');
+    document.querySelector('.title').classList.add('animate__animated');
+    document.querySelector('.title').classList.add('animate__backInDown');
+    document.querySelector('hr').classList.add('animate__animated');
+    document.querySelector('hr').classList.add('animate__backInRight');
+    document.querySelector('.clientId').classList.add('animate__animated');
+    document.querySelector('.clientId').classList.add('animate__backInLeft');
+    document.querySelector('.details').classList.add('animate__animated');
+    document.querySelector('.details').classList.add('animate__backInLeft');
+    document.querySelector('.state').classList.add('animate__animated');
+    document.querySelector('.state').classList.add('animate__backInRight');
+    document.querySelector('.largeImageKey').classList.add('animate__animated');
+    document.querySelector('.largeImageKey').classList.add('animate__backInLeft');
+    document.querySelector('.largeImageText').classList.add('animate__animated');
+    document.querySelector('.largeImageText').classList.add('animate__backInRight');
+    document.querySelector('.smallImageKey').classList.add('animate__animated');
+    document.querySelector('.smallImageKey').classList.add('animate__backInLeft');
+    document.querySelector('.smallImageText').classList.add('animate__animated');
+    document.querySelector('.smallImageText').classList.add('animate__backInRight');
+    document.querySelector('.timestamp').classList.add('animate__animated');
+    document.querySelector('.timestamp').classList.add('animate__backInRight');
+    document.querySelector('.connectButton').classList.add('animate__animated');
+    document.querySelector('.connectButton').classList.add('animate__backInUp');
+});
 document.querySelector('#btn-minimize').addEventListener('click', () => {
     if(remote.getCurrentWindow().isMinimized() == false){
         remote.getCurrentWindow().minimize();
@@ -8,41 +35,33 @@ document.querySelector('#btn-minimize').addEventListener('click', () => {
 document.querySelector('#btn-close').addEventListener('click', () => {
     remote.getCurrentWindow().close();
 });
-
-document.querySelector('.connectButton').addEventListener('click', () => {
+document.querySelector('.timestamp').addEventListener('click', (e) => {
+    let __tClassList = document.querySelector('.timestamp').classList;
+    if(document.querySelector('.timestamp').childNodes[1].checked == false){
+        __tClassList.remove('checkbox-active');
+    }else{
+        __tClassList.add('checkbox-active');
+    }
+});
+document.querySelector('.connectButton').addEventListener('click', async () => {
     if(document.querySelector('.clientId').value.length < 5){
         ipcRenderer.invoke('sendNotification', 'You have entered an invalid ID, the client could not be connected.');
     }else if(document.querySelector('.details').value.length < 1){
-        ipcRenderer.invoke('It is necessary that minimally, write something in the field "Details"');
+        ipcRenderer.invoke('sendNotification', 'It is necessary that minimally, write something in the field "Details"');
     }else{
-        console.log(document.querySelector('.state').value.length);
         let clientId = document.querySelector('.clientId').value, activityData = {
             details: document.querySelector('.details').value,
-            state: (document.querySelector('.state').value.length > 1) ? document.querySelector('.state').value : null,
+            state: document.querySelector('.state').value,
             largeImageKey: document.querySelector('.largeImageKey').value,
             largeImageText: document.querySelector('.largeImageText').value,
             smallImageKey: document.querySelector('.smallImageKey').value,
             smallImageText: document.querySelector('.smallImageText').value,
-            timestamp: false,
+            startTimestamp: false,
         }
-
-        if(document.querySelector('.timestamp-checkbox').style.backgroundColor == '#e23f17'){
-            timestamp = true;
+        if(document.querySelector('.timestamp').classList.contains('checkbox-active')){
+            activityData.startTimestamp = new Date().getTime();
         }
-        
-        ipcRenderer.invoke('connect', clientId, activityData).then(r => {
-            if(r == 'Could not connect'){
-                ipcRenderer.invoke('sendNotification', 'An error occurred in the course of creating the activity: '+r);
-            }else if(r == 'connection closed'){
-                ipcRenderer.invoke('sendNotification', 'An error occurred in the course of creating the activity: '+r);                
-            }else if(r == 'RPC_CONNECTION_TIMEOUT'){
-                ipcRenderer.invoke('sendNotification', 'An error occurred in the course of creating the activity: '+r);                
-            }else if(r == 'Failed in register activity'){
-                ipcRenderer.invoke('sendNotification', 'An error occurred in the course of creating the activity: '+r);                
-            }else if(r == true){
-                ipcRenderer.invoke('sendNotification', 'The client was successfully connected, and the activity was recorded.');                
-            }
-        });
+        await ipcRenderer.invoke('connect', clientId, activityData);
     }
 
 });
